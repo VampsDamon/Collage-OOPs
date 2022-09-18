e3426470a89f46ac6ec2518e7ebe8daa727c0006
VampsDamon <91468616+VampsDamon@users.noreply.github.com>
2022-09-18 23:58:57 +0530

    Add files via upload

---
+++ Lab 1/fibonacci.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    int a=0;
+    int b=1;
+    int c;
+    int n;
+    cout<<"Enter terms of fibonacci series :- ";
+    cin>>n;
+    cout<<a<<" "<<b<<" ";
+    for (int i = 2; i < n; i++)
+    {
+        c=a+b;
+        cout<<c<<" ";
+        a=b;
+        b=c;
+    }
+    cout<<endl;
+    return 0;
+}
