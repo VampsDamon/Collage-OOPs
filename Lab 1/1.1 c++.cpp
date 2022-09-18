8b621608d10068adbf984cc0b43bfe4f5a9d189a
VampsDamon <91468616+VampsDamon@users.noreply.github.com>
2022-09-18 23:58:09 +0530

    Create 1.1 c++.cpp

---
+++ Lab 1/1.1 c++.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void check(int a)
+{
+    cout<<"C++ is better then C\n";
+}
+void check(char a)
+{
+    cout<<"Enter valid input...!\n";
+}
+int main()
+{
+    int a;
+    cout<<"Enter a integer :-- ";
+    b:
+    cin>>a;
+    check(a);
+      
+    return 0;
+}
