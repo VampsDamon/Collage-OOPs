e4ec61fe81d09af1f34bb94f36bfb492205c79af
VampsDamon <91468616+VampsDamon@users.noreply.github.com>
2022-09-19 00:03:04 +0530

    Create 4.1 Greeting.cpp

---
+++ Lab 4/4.1 Greeting.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class sid
+{
+   public:
+   void print()
+   {
+      cout<<"Hello Shahid !..."<<endl;
+   }
+};
+
+int main()
+{
+    sid a;
+    a.print();
+    return 0;
+}
